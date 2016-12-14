0ef912618bc861c70624bd7469d118b9efb0045f
Sonam Pasi <sonampasips@gmail.com>
2016-12-14 15:28:45 +0530

    simple implementation of array sort

---
+++ src/sortArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class sortArray {
+	
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		//enter the array size
+		int n = s.nextInt();
+		//initialize the array
+		int array[] = new int[n];
+		//enter elements in the array
+		for(int i = 0; i < n; i++){
+			array[i] = s.nextInt();
+		}
+		//sort array in ascending order
+		for(int i = 0; i < array.length; i++ ){
+			for(int j = i+1; j < array.length; j++){
+				if(array[i] > array[j]){
+					int temp = array[j];
+					array[j] = array[i];
+					array[i] = temp;
+				}
+			}
+		}
+		//print sorted array
+		for(int i = 0; i < n; i++){
+			System.out.print(array[i] + " ");
+		}
+	}
+}
