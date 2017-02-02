da3e4785fd58be64861501ad49acdaa7e0948f6a
Sonam Pasi <sonampasips@gmail.com>
2017-02-02 22:51:27 +0530

    removed left right counter

--- src/Parentheses.java
+++ src/Parentheses.java
@@ -14,36 +14,38 @@ public class Parentheses {
 			if (c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']') {
 				// if stack is not empty and character is right parenthesis, pop
 				// the top element
-				if (c == ')'|| c == '}' || c == ']') {
-					if (!stack.empty() && ((stack.peek()=='(' && c == ')') || (stack.peek()=='{' && c == '}') || (stack.peek()=='[' && c == ']'))) {
+				if (c == ')' || c == '}' || c == ']') {
+					if (stack.empty()) {
+						return false;
+					}
+					if ((stack.peek() == '(' && c == ')') || (stack.peek() == '{' && c == '}')
+							|| (stack.peek() == '[' && c == ']')) {
 						stack.pop();
 					}
-					right++;
 				}
 				// only push left parenthesis into the stack
 				else {
 					stack.push(c);
-					left++;
 				}
 			}
 			i++;
 		}
 		// if stack is empty, all the parenthesis are in matched pair return
 		// true
-		return (stack.empty() && (right == left));
+		return stack.empty();
 	}
 
 	public static void main(String args[]) {
 		Parentheses p = new Parentheses();
-		String s1 = "(xyz(abcdo)lkj){asdf}[(456)]";	//true
+		String s1 = "(xyz(abcdo)lkj){asdf}[(456)]"; // true
 		System.out.println(p.isValid(s1));
-		String s2 = "[(xyz(abcdo)lkj){asdf}[(456)]]]]]]";	//false
+		String s2 = "[(xyz(abcdo)lkj){asdf}[(456)]]]]]]"; // false
 		System.out.println(p.isValid(s2));
-		String s3 = "(((((xyz(abcdo)lkj){asdf}[(456)]"; //false
+		String s3 = "(((((xyz(abcdo)lkj){asdf}[(456)]"; // false
 		System.out.println(p.isValid(s3));
-		String s4 = "([456)]";	//false
+		String s4 = "([456)]"; // false
 		System.out.println(p.isValid(s4));
-		String s5 = "()";	//true
+		String s5 = "()"; // true
 		System.out.println(p.isValid(s5));
 	}
 }
