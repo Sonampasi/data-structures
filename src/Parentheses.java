595362712d2739cd1dc04eba70ba085d11326f55
Sonam Pasi <sonampasips@gmail.com>
2017-02-02 15:00:55 +0530

    fixing bugs

--- src/Parentheses.java
+++ src/Parentheses.java
@@ -1,21 +1,21 @@
 import java.util.Stack;
 
 public class Parentheses {
-	Stack<Character> stack = new Stack();
 
 	public boolean isValid(String s) {
 		int i = 0;
 		char c;
 		int right = 0;
 		int left = 0;
+		Stack<Character> stack = new Stack<Character>();
 		while (i < s.length()) {
 			c = s.charAt(i);
 			// consider only for parentheses characters
 			if (c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']') {
 				// if stack is not empty and character is right parenthesis, pop
 				// the top element
-				if (c == ')' || c == '}' || c == ']') {
-					if (!stack.empty()) {
+				if (c == ')'|| c == '}' || c == ']') {
+					if (!stack.empty() && ((stack.peek()=='(' && c == ')') || (stack.peek()=='{' && c == '}') || (stack.peek()=='[' && c == ']'))) {
 						stack.pop();
 					}
 					right++;
@@ -35,7 +35,15 @@ public class Parentheses {
 
 	public static void main(String args[]) {
 		Parentheses p = new Parentheses();
-		String s = "(xyz(abcdo)lkj){asdf}[(456)]";
-		System.out.print(p.isValid(s));
+		String s1 = "(xyz(abcdo)lkj){asdf}[(456)]";	//true
+		System.out.println(p.isValid(s1));
+		String s2 = "[(xyz(abcdo)lkj){asdf}[(456)]]]]]]";	//false
+		System.out.println(p.isValid(s2));
+		String s3 = "(((((xyz(abcdo)lkj){asdf}[(456)]"; //false
+		System.out.println(p.isValid(s3));
+		String s4 = "([456)]";	//false
+		System.out.println(p.isValid(s4));
+		String s5 = "()";	//true
+		System.out.println(p.isValid(s5));
 	}
 }
