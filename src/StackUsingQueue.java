a96c0781c5ad3c4eab1508d9253f9066cc9e812f
Sonam Pasi <sonampasips@gmail.com>
2017-02-04 01:34:23 +0530

    Fixing bugs

--- src/StackUsingQueue.java
+++ src/StackUsingQueue.java
@@ -1,60 +1,73 @@
+import java.util.Queue;
 import java.util.LinkedList;
-import java.util.Queue;;
 
 public class StackUsingQueue {
-	
-	Queue<Integer> queue;
 
-    public StackUsingQueue() {
-    	queue = new LinkedList();
-    }
-    
-    /** Push element x onto stack. */
-    public void push(int x) {
-    	 queue.add(x);
-    }
-    
-    /** Removes the element on top of the stack and returns that element. */
-    public int pop(){
-    	int current = queue.peek();
-    	Queue<Integer> tempQueue = new LinkedList<Integer>();
-    	if(queue.isEmpty()){
-    		return -1;
-    	}
-    	for(int i = 1; i < queue.size(); i++){	
-    		tempQueue.add(current);
-    		current = (int) ((LinkedList<Integer>) queue).get(i);
-    	}
-    	queue = tempQueue;
-    	return current;
-    }
-    
-    /** Get the top element. */
-    public int top() {
-    	if(!queue.isEmpty()){
-    		int current = queue.peek();
-    		for(int i = 0; i < queue.size(); i++){	
-        		current = (int) ((LinkedList<Integer>) queue).get(i);
-        	}
-    		return current;
-    	}else{
-    		return -1;
-    	}
-    }
-    
-    /** Returns whether the stack is empty. */
-    public boolean empty() {
-    	return(queue.size() == 0);
-    }
-    
-    public static void main(String args[]) {
+	Queue<Integer> queue;
+	Integer rear;
+
+	public StackUsingQueue() {
+		queue = new LinkedList();
+		rear = null;
+	}
+
+	/** Push element x onto stack. */
+	public void push(int x) {
+		queue.add(x);
+		rear = x;
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	public int pop() {
+		if (queue.isEmpty()) {
+			return -1;
+		}
+		Integer poppedItem = null;
+		Integer temp = null;
+		int i = 0;
+		try {
+			while (!queue.isEmpty()) {
+				poppedItem = queue.remove();
+				temp = poppedItem;
+				if (i >= queue.size()) {
+					return temp;
+				}
+				queue.add(temp);
+				rear = temp;
+				i++;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return poppedItem;
+	}
+
+	/** Get the top element. */
+	public int top() {
+		if (queue.isEmpty()) {
+			return -1;
+		} else {
+			return rear;
+		}
+	}
+
+	/** Returns whether the stack is empty. */
+	public boolean empty() {
+		return (queue.isEmpty());
+	}
+
+	public static void main(String args[]) {
 		StackUsingQueue obj = new StackUsingQueue();
 		obj.push(23);
-		//obj.push(6);
-		System.out.println(obj.pop());
-		int param_3 = obj.top();
-		System.out.println(param_3);
+		obj.push(11);
+		obj.push(61);
+		obj.push(61);
+		System.out.println("Pop: " + obj.pop());
+		System.out.println("Pop: " + obj.pop());
+		System.out.println("Pop: " + obj.pop());
+		System.out.println("Top: " + obj.top());
 		boolean param_4 = obj.empty();
 		System.out.println(param_4);
-    }
+	}
 }
